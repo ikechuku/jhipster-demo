067031eb2e9edb0edbf294f62f266b86d2cffc1b
ikechuku <rockstarik2@gmail.com>
2020-03-19 20:40:07 +0100

    add config for Kotlin and Node

---
+++ bouncer/api/views/jhipster/create_file.py
@@ -0,0 +1,62 @@
+import json
+
+def create_file(raw_data):
+        # true = "true"
+        # false = "false"
+        output = {
+                "generator-jhipster": {
+                "promptValues": {
+                "packageName": "org.jhipster.blog",
+                "nativeLanguage": "en"
+                },
+
+                }
+                }
+        for field in raw_data["fields"]:
+                if field['name']  == "applicationType" :
+                        output["generator-jhipster"]["applicationType"] = field["string"]
+
+                if field['name']  == "baseName" :
+                        output["generator-jhipster"]["baseName"] = field["string"]
+
+                if field['name']  == "packageName" :
+                        output["generator-jhipster"]["packageName"] = field["string"]
+
+                if field['name']  == "authenticationType" :
+                        output["generator-jhipster"]["authenticationType"] = field["string"]
+                        
+                if field['name']  == "databaseType" :
+                        output["generator-jhipster"]["databaseType"] = field["string"]
+
+                if field['name']  == "devDatabaseType" :
+                        output["generator-jhipster"]["devDatabaseType"] = field["string"]
+
+                if field['name']  == "prodDatabaseType" :
+                        output["generator-jhipster"]["prodDatabaseType"] = field["string"]
+
+                if field['name']  == "buildTool" :
+                        output["generator-jhipster"]["buildTool"] = field["string"]
+                
+                if field['name']  == "clientFramework" :
+                        output["generator-jhipster"]["clientFramework"] = field["string"]
+
+                if field["name"] == "progLang":
+                        if field["string"] == "Kotlin":
+                                output["generator-jhipster"]["otherModules"] =  [
+                                {
+                                        "name": "generator-jhipster-kotlin",
+                                        "version": "1.6.0"
+                                }
+                                ]
+                if field["name"] == "progLang":
+                        if field["string"] == "Node":
+                                output["generator-jhipster"]["otherModules"] =  [
+                                {
+                                        "name": "generator-jhipster-node",
+                                }
+                                ]
+
+        with open('.yo-rc.json', 'w') as outfile:
+                json.dump(output, outfile)
+        
+        return output
--- bouncer/api/views/jhipster/jhipster_view.py
+++ bouncer/api/views/jhipster/jhipster_view.py
@@ -1,47 +1,15 @@
-import json
+from .create_file import create_file
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
 
 class JhipsterView(APIView):
-    def post(self, request):
-        raw_data = request.data
-        try:
+        def post(self, request):
+                data = request.data
 
-            for field in raw_data["fields"]:
-                if field['name']  == "applicationType" :
-                    output["generator-jhipster"]["applicationType"] = field["string"]
+                try:
+                        return Response(create_file(data))
 
-                if field['name']  == "baseName" :
-                    output["generator-jhipster"]["baseName"] = field["string"]
-
-                if field['name']  == "packageName" :
-                        output["generator-jhipster"]["packageName"] = field["string"]
-
-                if field['name']  == "authenticationType" :
-                        output["generator-jhipster"]["authenticationType"] = field["string"]
-                        
-                if field['name']  == "databaseType" :
-                        output["generator-jhipster"]["databaseType"] = field["string"]
-
-                if field['name']  == "devDatabaseType" :
-                        output["generator-jhipster"]["devDatabaseType"] = field["string"]
-
-                if field['name']  == "prodDatabaseType" :
-                        output["generator-jhipster"]["prodDatabaseType"] = field["string"]
-
-                if field['name']  == "buildTool" :
-                        output["generator-jhipster"]["buildTool"] = field["string"]
-                
-                if field['name']  == "clientFramework" :
-                        output["generator-jhipster"]["clientFramework"] = field["string"]
-
-
-                with open('.yo-rc.json', 'w') as outfile:   
-                    json.dump(output, outfile)
-
-
-            return Response(data  )
-        except:
-            return Response(dict(error="I couldn't get valid data"), status=status.HTTP_400_BAD_REQUEST)
+                except:
+                        return Response(dict(error="OOPS!, Something went wrong"), status=status.HTTP_400_BAD_REQUEST)
